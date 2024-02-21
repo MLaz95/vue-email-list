8bacbdbcb213fd7e7adcbd49bcfc46463920c461
marco <lazzari.marco.ml@gmail.com>
2024-02-21 14:32:19 +0100

    generato array di email con api

--- script.js
+++ script.js
@@ -3,7 +3,25 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        
+        // stores the generated email addresses
+        emailArray: [],
       }
+    },
+
+    methods: {
+        // gets an email address from the api and adds it to emailArray
+        getEmail() {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((mail) => {
+            this.emailArray.push(mail.data.response);
+            })
+        }
+    },
+
+    mounted(){
+        // runs getEmail 10 times
+        for(i = 0; i < 10; i++){
+            this.getEmail()  
+        }
+        console.log(this.emailArray);
     }
   }).mount('#app')
